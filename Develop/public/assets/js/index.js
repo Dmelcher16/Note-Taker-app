87e7612d49d8819ea8ba4227e4e79f5dc6aa51f8
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-09-04 23:03:42 -0700

    added npm packages

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -3,6 +3,10 @@ const $noteText = $(".note-textarea");
 const $saveNoteBtn = $(".save-note");
 const $newNoteBtn = $(".new-note");
 const $noteList = $(".list-container .list-group");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
 
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
